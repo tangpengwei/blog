7ad8cca587e9bef6ffbefee6eae4d1a5057c9052
tangpengwei <528584252@qq.com>
2019-05-31 21:27:17 +0800

    进一步完善

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -47,11 +47,6 @@ class Index extends Controller
 
         $info =  Interview::with('author')->where('company_id',$id)->paginate(10);
         $this->assign('info',$info);
-//        $allInfo =[];
-//        foreach ($info as $v){
-//            $allInfo[]=$v;
-//        }
-//        print_r($allInfo[0]);
         return $this->fetch();
     }
 
@@ -110,28 +105,28 @@ class Index extends Controller
             'company_id' => 'require'
         ];
         $msg = [
-            'content.require' => '面试经历详情为必填项',
-            'content.length' => '面试经历长度错误',
-            'company_id.require' => '企业信息不全'
+            'content.require' => '评论详情为必填项',
+            'content.length' => '评论长度错误',
+            'company_id.require' => '评论信息不全'
         ];
         if ($this->validate($data,$rule,$msg) === true){
             //验证企业的有效性
             $company = \think\Db::table('article')->where('id',$data['company_id'])->find();
             if (empty($company)){
-                $this->error('企业不存在');
+                $this->error('评论不存在');
             }
             //记录发表者的信息
             $data['uid'] = $user->id;
             //记录添加时间
             $data['create_time'] = date('Y-m-d H:i:s');
             if (\think\Db::table('interview')->data($data)->insert()){
-                $this->success('添加成功1');
+                $this->success('添加成功');
             }else{
-                $this->error('添加失败2');
+                $this->error('添加失败');
             }
 
         }else{
-            $this->error('添加失败3');
+            $this->error($this->validate($data,$rule,$msg));
         }
 
     }
@@ -147,4 +142,66 @@ class Index extends Controller
         $this->assign('Info',$Info);
         return $this->fetch();
     }
+
+
+    /**
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * @throws \think\exception\PDOException
+     * 判断点赞
+     */
+
+    public function ding(){
+        //用户必须登录
+        //当前登录用户的信息
+        $user = session('userloginInfo');
+        if (empty($user)){
+            $this->error('你需要登录后才能操作');
+        }
+        //这是面试经历的id
+        $id = $this->request->param('id');
+        //数据验证
+        if (empty($id)){
+            $this->error('非法操作');
+        }
+        //一条面试经历的信息
+        $info = \think\Db::table('interview')->where('id',$id)->find();
+        //检验面试信息是否为null
+        if (empty($info)){
+            $this->error('非法操作');
+        }
+        //检验是否是给自己点赞
+        if ($info['uid'] == $user->id){
+            $this->error('不要自己给自己点赞');
+        }
+        //检验用户是否已经点赞过
+        if (\think\Db::table('interview_ding')->where(['interview_id'=>$id, 'uid'=>$user->id])->find()){
+
+
+            //取消赞
+            if (\think\Db::table('interview')->where('id',$id)->setDec('ding')){
+                \think\Db::table('interview_ding')->where(['interview_id' => $id,'uid'=>$user->id])->delete();
+                $this->success('取消成功',null,1);
+            }else{
+                $this->error('操作失败');
+            }
+        }else{
+            if (\think\Db::table('interview')->where('id',$id)->setInc('ding')){
+                \think\Db::table('interview_ding')->data(['interview_id'=>$id,'uid'=>$user->id])->insert();
+                $this->success('点赞成功',null,0);
+            }else{
+                $this->error('操作失败');
+            }
+        }
+
+
+    }
+
+
+
+
+
+
 }
